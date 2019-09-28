4a1c8fa4d2c27fa8ce27036db64bc34315ce29d4
yangjikang <470294123@qq.com>
2019-09-28 22:18:25 +0800

    1.0V

---
+++ src/main/java/com/jikang/config/MyShiroRealm.java
@@ -0,0 +1,52 @@
+package com.jikang.config;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+public class MyShiroRealm extends AuthorizingRealm {
+    /**
+     * 角色权限和对应权限添加
+     * @param principalCollection
+     * @return
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        System.out.println("==>角色权限和对应权限添加");
+        //获取登录用户名
+        String name = (String) principalCollection.getPrimaryPrincipal();
+        //添加角色和权限
+        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+
+        //添加角色
+        simpleAuthorizationInfo.addRole("admin");
+
+        //添加权限
+        simpleAuthorizationInfo.addStringPermission("create");
+
+        return simpleAuthorizationInfo;
+    }
+
+    /**
+     * 用户认证
+     * @param authenticationToken
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        System.out.println("==>用户认证");
+        UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
+
+        String name = token.getUsername();
+
+        if (name == null) {
+            return null;
+        }
+        //这里验证authenticationToken和simpleAuthenticationInfo的信息
+        SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(name, "123456", getName());
+        return simpleAuthenticationInfo;
+    }
+}
